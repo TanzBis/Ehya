b32212f1839b3d62a038640d18a795c058e4728d
TanzBis <tanzbis81@gmail.com>
2021-12-15 05:35:51 +0300

    create: section with reccomend books

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// var menuButton = document.querySelector(".navbar-button")
+// menuButton.addEventListener('click', function () {
+//   console.log('Клик по кнопке меню')
+//   document.querySelector(".navbar__nav")
+//   .classList.toggle('navbar__nav--visible');
+// })
