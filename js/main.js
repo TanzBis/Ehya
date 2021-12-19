1c7a6a1536ee737481a000f3923466072c91f919
TanzBis <tanzbis81@gmail.com>
2021-12-20 01:11:40 +0300

    create: added slider-container

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.categories__button--next',
+    prevEl: '.categories__button--prev',
+  },
+});
+
 // var menuButton = document.querySelector(".navbar-button")
 // menuButton.addEventListener('click', function () {
 //   console.log('Клик по кнопке меню')
