619a07e2b4233b3e120b79ccad1392673f952ad2
TanzBis <tanzbis81@gmail.com>
2021-12-21 03:28:30 +0300

    fix: added seecond slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper(".categories-slider", {
-  loop: true,
+  loop: false,
   clickable: true,
   
   navigation: {
@@ -7,7 +7,45 @@ var swiper = new Swiper(".categories-slider", {
     prevEl: '.categories__button--next',
   },
 });
+
+var swiper = new Swiper(".unpublished-slider", {
+  loop: false,
+  clickable: true,
+  
+  navigation: {
+    nextEl: '.unpublished__button--next',
+    prevEl: '.unpublished__button--prev',
+  },
+
+  breakpoints: {
+    1440: {
+      spaceBetween: 0,
+      slidesPerView: 5,
+    },
+    1200: {
+        spaceBetween: 10,
+        slidesPerView: 5,
+    },
+    992: {
+      spaceBetween: 10,
+      slidesPerView: 4,
+    },
+    767: {
+      spaceBetween: 10,
+      slidesPerView: 3,
+    },
+    576: {
+      spaceBetween: 10,
+      slidesPerView: 3,
+    },
+    480: {
+      spaceBetween: 10,
+      slidesPerView: 1,
+    },
+  },
+});
     
+
 
 // var menuButton = document.querySelector(".navbar-button")
 // menuButton.addEventListener('click', function () {
